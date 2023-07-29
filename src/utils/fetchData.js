b7c149edcd069bce87d3389f58de7c21079e8700
hitesh <yadavhitesh335@gmail.com>
2023-07-29 14:00:20 +0530

    v1

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '66ba9f97f8msh848c9be3c7595eep166d5ejsnb405f5193b88',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
